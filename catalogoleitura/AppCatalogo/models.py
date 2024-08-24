cde10067286bb603e6925e14079d80e52a2c931e
JoaoVitorMartelleto <joao.m.d.p.t@gmail.com>
2024-08-24 11:05:05 -0300

    criação dos models
    Fiz a estrutura básica dos models que foram/são utilizados nessa aplicação.

--- catalogoleitura/AppCatalogo/models.py
+++ catalogoleitura/AppCatalogo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    data_publi = models.DateField()
+    lido = models.BooleanField(default=False)
+    nota = models.FloatField(null=True, blank=True)
+    comentarios = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.titulo
+    
+    
+class Usuario (models.Model):
+    usuario = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    livro = models.ForeignKey(Livro,  on_delete=models.CASCADE)
+ 
+    def __str__(self):
+        return self.nome   
